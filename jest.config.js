980154d06c155ac95d200fe78af9fff8d74022a2
Leonardo Ferreira <leonardo.rocha0803@gmail.com>
2025-04-09 09:59:46 -0300

    add jest coverage config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: [
+    '**/*.(t|j)s',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/coverage/**',
+    '!**/*.module.ts',
+    '!**/guards/**',
+    '!**/strategies/**',
+    '!**/middlewares/**',
+    '!**/entities/**',
+    '!main.ts',
+  ],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+};
